ccb3eb6ef3fe1a5d88706da603a9b7f598d4be91
OrlandoYV <sergioyv@gmail.com>
2023-10-19 00:03:39 -0500

    oy - Products DB creation and Migration

---
+++ GearBox.DataAccess/Migrations/20231019045900_addProductsToDb.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace GearBox.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class addProductsToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Categories",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ISBN = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Author = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ListPrice = table.Column<double>(type: "float", nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false),
+                    Price50 = table.Column<double>(type: "float", nullable: false),
+                    Price100 = table.Column<double>(type: "float", nullable: false),
+                    ProductDiscount = table.Column<double>(type: "float", nullable: false),
+                    CostOfProduct = table.Column<double>(type: "float", nullable: false),
+                    Weight = table.Column<double>(type: "float", nullable: false),
+                    Large = table.Column<double>(type: "float", nullable: false),
+                    Width = table.Column<double>(type: "float", nullable: false),
+                    Height = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Author", "CostOfProduct", "Description", "Height", "ISBN", "Large", "ListPrice", "Price", "Price100", "Price50", "ProductDiscount", "Title", "Weight", "Width" },
+                values: new object[,]
+                {
+                    { 1, "Billy Spark", 0.0, "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ", 0.0, "SWD9999001", 0.0, 99.0, 90.0, 80.0, 85.0, 0.0, "Fortune of Time", 0.0, 0.0 },
+                    { 2, "Nancy Hoover", 0.0, "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ", 0.0, "CAW777777701", 0.0, 40.0, 30.0, 20.0, 25.0, 0.0, "Dark Skies", 0.0, 0.0 },
+                    { 3, "Julian Button", 0.0, "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ", 0.0, "RITO5555501", 0.0, 55.0, 50.0, 35.0, 40.0, 0.0, "Vanish in the Sunset", 0.0, 0.0 },
+                    { 4, "Abby Muscles", 0.0, "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ", 0.0, "WS3333333301", 0.0, 70.0, 65.0, 55.0, 60.0, 0.0, "Cotton Candy", 0.0, 0.0 },
+                    { 5, "Ron Parker", 0.0, "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ", 0.0, "SOTJ1111111101", 0.0, 30.0, 27.0, 20.0, 25.0, 0.0, "Rock in the Ocean", 0.0, 0.0 },
+                    { 6, "Laura Phantom", 0.0, "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ", 0.0, "FOT000000001", 0.0, 25.0, 23.0, 20.0, 22.0, 0.0, "Leaves and Wonders", 0.0, 0.0 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Categories",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(30)",
+                oldMaxLength: 30);
+        }
+    }
+}
