a8134b54c407d7fdb8cbeaacb52905f1735762e0
OrlandoYV <sergioyv@gmail.com>
2023-10-24 00:01:06 -0500

    oy - home page layout , cards

--- GearBoxWeb/Areas/Customer/Controllers/HomeController.cs
+++ GearBoxWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using GearBox.DataAccess.Repository.IRepository;
 using GearBox.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -8,15 +9,18 @@ namespace GearBoxWeb.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category");
+            return View(productList);
         }
 
         public IActionResult Privacy()
